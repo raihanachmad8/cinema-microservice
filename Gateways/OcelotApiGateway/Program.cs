754c1a475ab5bb1ce0e9ca082f18bb050e660565
Achmad Raihan Fahrezi Effendy <raihanachmad8@gmail.com>
2025-02-27 08:38:31 +0700

    feat(gateway): update OcelotApiGateway configuration and Docker setup

--- Gateways/OcelotApiGateway/Program.cs
+++ Gateways/OcelotApiGateway/Program.cs
@@ -1,34 +1,43 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using DotNetEnv;
 
-namespace OcelotApiGateway
+var builder = WebApplication.CreateBuilder(args);
+
+// Load environment variables from Config/global.env
+Env.Load("../../../Configs/global.env");
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Add Ocelot services
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+Console.WriteLine(app.Environment.IsDevelopment());
+
+// Use developer exception page in development mode
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            // Load environment variables from Config/global.env
-            Env.Load("../../Configs/global.env"); // or specify the path where your global.env file is located
-
-            // Start the application
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    config.SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    var environmentUrls = Environment.GetEnvironmentVariable("API_GATEWAY_HOST") ?? "http://localhost:5000";
-                    webBuilder.UseUrls(environmentUrls);
-                });
-    }
-
+    app.UseDeveloperExceptionPage();
 }
+
+// Routing middleware
+app.UseRouting();
+
+// Authorization middleware
+app.UseAuthorization();
+
+// Enable Ocelot middleware to forward requests
+await app.UseOcelot();
+
+// Get environment-specific URLs
+var environmentUrls = Environment.GetEnvironmentVariable("API_GATEWAY_HOST") ?? "http://localhost:5000";
+app.Urls.Add(environmentUrls);
+
+app.Run();
