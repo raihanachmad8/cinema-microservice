023f01a82a2ac9835f342e58b9c15f4f69b911eb
raihanachmad8 <raihanachmad8@gmail.com>
2025-03-04 18:18:43 +0700

    feat(Domain, Infrastructure): add domain infra extension for migration and seeder

---
+++ Services/MovieService/Domain/Enums/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieService.Domain.Enums;
+public enum Genre
+{
+    Action = 1,
+    Drama = 2,
+    Comedy = 3,
+    Horror = 4,
+    Romance = 5,
+    Thriller = 6,
+    SciFi = 7
+}
