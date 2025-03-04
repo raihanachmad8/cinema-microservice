4e4dfe8596ae5380c164eded20da7c71d1cbeb67
raihanachmad8 <raihanachmad8@gmail.com>
2025-03-04 20:38:25 +0700

    feat(movie): add repository, DTOs, and logging
    - Added MovieQueryParams and MovieRequest DTOs for movie-related requests.
    - Implemented MoviePaginateResponse and Metadata for pagination responses.
    - Created IMovieRepository interface for movie repository operations.
    - Added LoggerService for logging functionality.
    - Updated MovieRepository for movie data access.
    - Modified Program.cs and launchSettings.json for integration.

---
+++ Services/MovieService/Application/Interfaces/Repository/IMovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using MovieService.Application.DTOs.Responses;
+using MovieService.Domain.Entities;
+
+namespace MovieService.Application.Interfaces.Repositories;
+
+public interface IMovieRepository
+{
+    Task<Movie?> GetByIdAsync(Guid id);
+    Task<Movie?> GetByTitleAsync(string title);
+    Task<IEnumerable<Movie>> GetAllAsync();
+    Task<IEnumerable<Movie>> SearchAsync(string searchTerm);
+    Task AddAsync(Movie movie);
+    Task UpdateAsync(Movie movie);
+    Task DeleteAsync(Guid id);
+    Task<MoviePaginateResponse> GetMoviesAsync(string search, string orderBy, string? sort, int page, int pageSize);
+}
