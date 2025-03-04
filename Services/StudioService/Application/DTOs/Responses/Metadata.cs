b2c1c6e13722dcbb847daa716cfeef521def8268
raihanachmad8 <raihanachmad8@gmail.com>
2025-03-04 16:20:56 +0700

    fix: resolve redundant metadata

--- Services/StudioService/Application/DTOs/Responses/Metadata.cs
+++ Services/StudioService/Application/DTOs/Responses/Metadata.cs
@@ -2,11 +2,9 @@
 
 public record Metadata
 {
-    public int FirstPageNumber => TotalPages > 0 ? 1 : 0;
-    public int LastPageNumber => TotalPages;
     public int Page { get; set; }
     public int PageSize { get; set; }
-    public int TotalPages { get; set; }
+    public int TotalPages { get; }
     public int TotalRecords { get; set; }
 
     public Metadata(int page, int pageSize, int totalRecords)
@@ -16,4 +14,8 @@ public record Metadata
         TotalRecords = totalRecords;
         TotalPages = (int)Math.Ceiling((double)totalRecords / pageSize);
     }
+
+    public int GetFirstPageNumber() => TotalRecords > 0 ? 1 : 0;
+
+    public int GetLastPageNumber() => TotalPages > 0 ? TotalPages : 0;
 }
